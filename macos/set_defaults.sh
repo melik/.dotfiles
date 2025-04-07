87f952639459a9b05bcf3eaf62629c90c9abf05e
Alexey M <135643+melik@users.noreply.github.com>
2025-04-07 14:35:12 +0300

    Update Macos default settings

--- macos/set_defaults.sh
+++ macos/set_defaults.sh
@@ -1,41 +1,19 @@
 # The original idea (and a couple settings) were grabbed from:
 # https://github.com/mathiasbynens/dotfiles/blob/master/.macos
 # https://macos-defaults.com/
+# https://developer.apple.com/documentation/devicemanagement/dock
+# https://mynixos.com/nix-darwin/options/system.defaults
 
-# Finder
-# Always open everything in Finder's column view. This is important.
-sudo defaults write com.apple.Finder FXPreferredViewStyle clmv
-# Set the Finder prefs for showing a few different volumes on the Desktop.
-sudo defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
-sudo defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
-sudo defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
-# Keep folders on top
-sudo defaults write com.apple.finder _FXSortFoldersFirst -bool true
-# Show pathbar with paths
-sudo defaults write com.apple.finder "ShowPathbar" -bool "true"
-
-
-
-# Safari
-# Set up for development.
-sudo defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
-sudo defaults write com.apple.Safari IncludeDevelopMenu -bool true
-sudo defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-sudo defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
-sudo defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
-# Show full website URL
-sudo defaults write com.apple.safari ShowFullURLInSmartSearchField -bool true
 
 # Dock
 # Icons size of 36 pixels.
-sudo defaults write com.apple.dock tilesize -int 36
+defaults write com.apple.dock "tilesize" -int "36"
 # Display recent apps
-sudo defaults write com.apple.dock show-recents -bool false
+defaults write com.apple.dock "show-recents" -bool "false"
 # Put the Dock on the left of the screen
-sudo defaults write com.apple.dock orientation -string left
-
-# Enable AirDrop over Ethernet and on unsupported Macs running Lion
-# sudo defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
+defaults write com.apple.dock "orientation" -string "left"
+# Enable Maginification
+defaults write com.apple.dock "magnification" -bool "true"
 
 # Hot corners
 # Possible values:
@@ -51,23 +29,74 @@ sudo defaults write com.apple.dock orientation -string left
 # 12: Notification Center
 # 13: Lock Screen
 # Top left screen corner → Start screen saver
-sudo defaults write com.apple.dock wvous-tl-corner -int 5
-sudo defaults write com.apple.dock wvous-tl-modifier -int 0
+defaults write com.apple.dock wvous-tl-corner -int "5"
+defaults write com.apple.dock wvous-tl-modifier -int "0"
 # Bottom left screen corner → Desktop
-sudo defaults write com.apple.dock wvous-bl-corner -int 4
-sudo defaults write com.apple.dock wvous-bl-modifier -int 0
+defaults write com.apple.dock wvous-bl-corner -int "0"
+defaults write com.apple.dock wvous-bl-modifier -int "0"
+
+
+# Finder
+# Always open everything in Finder's column view. This is important
+defaults write com.apple.finder "FXPreferredViewStyle" -string "clmv"
+# Set the Finder prefs for showing a few different volumes on the Desktop.
+defaults write com.apple.finder "ShowHardDrivesOnDesktop" -bool "true"
+defaults write com.apple.finder "ShowExternalHardDrivesOnDesktop" -bool "true"
+defaults write com.apple.finder "ShowRemovableMediaOnDesktop" -bool "true"
+defaults write com.apple.finder "ShowMountedServersOnDesktop" -bool "true"
+# Keep folders on top
+defaults write com.apple.finder "_FXSortFoldersFirst" -bool "true"
+# Show pathbar with path
+defaults write com.apple.finder "ShowPathbar" -bool "true"
+# Set default path for new windows
+defaults write com.apple.finder "NewWindowTarget" -string "PfHm"
+# Hide tags in sidebar
+defaults write com.apple.finder ShowRecentTags -bool "false"
+
+
+# Enable AirDrop over Ethernet and on unsupported Macs running Lion
+# sudo defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
 
 
 # Trackpad
 # Click weight (threshold) - set to medium
-sudo defaults write com.apple.AppleMultitouchTrackpad "FirstClickThreshold" -int "1"
+defaults write com.apple.AppleMultitouchTrackpad "FirstClickThreshold" -int "1"
+# Tap to click
+defaults write com.apple.AppleMultitouchTrackpad "Clicking" -int "1"
 
 
 # Desktop Services
-# Avoid creating .DS_Store files on network or USB volumes
-sudo defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-# Avoid creating .DS_Store files on network or USB volumes
-sudo defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+# Avoid creating .DS_Store files on network volumes
+sudo defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool "true"
+# Avoid creating .DS_Store files on USB volumes
+sudo defaults write com.apple.desktopservices DSDontWriteUSBStores -bool "true"
+
+
+# Automatically quit printer app once the print jobs complete.
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool "true"
+
+# Music song notifications will not be displayed
+defaults write com.apple.Music "userWantsPlaybackNotifications" -bool "false"
+
+
+# Safari
+# Prevent Safari from opening ‘safe’ files automatically after downloading
+sudo defaults write com.apple.Safari "DownloadsClearingPolicy" -int "0"
+sudo defaults write com.apple.Safari AutoOpenSafeDownloads -bool "false"
+
+# Set up Default encoding
+sudo defaults write com.apple.Safari WebKitDefaultTextEncodingName -string 'utf-8'
+sudo defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DefaultTextEncodingName -string 'utf-8'
+
+# Set up for development.
+sudo defaults write com.apple.Safari "IncludeInternalDebugMenu" -bool "true"
+sudo defaults write com.apple.Safari "IncludeDevelopMenu" -bool "true"
+sudo defaults write com.apple.Safari "WebKitDeveloperExtrasEnabledPreferenceKey" -bool "true"
+sudo defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool "true"
+defaults write NSGlobalDomain "WebKitDeveloperExtras" -bool "true"
+
+# Show full website URL
+sudo defaults write com.apple.Safari "ShowFullURLInSmartSearchField" -bool "true"
 
 
 # Kill affected applications
@@ -75,6 +104,7 @@ for app in "cfprefsd" \
     "Dock" \
     "Finder" \
     "Safari" \
+    "Music" \
     "Spectacle" \
     "SystemUIServer"; do
     killall "${app}" &> /dev/null
